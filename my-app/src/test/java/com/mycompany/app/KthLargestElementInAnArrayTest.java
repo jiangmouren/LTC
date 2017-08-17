81c970d6203b68c717f2c919090927d942a2a73a
jiangmouren <jiangmouren@gmail.com>
2017-08-17 10:57:45 -0700

    tmp commit

---
+++ my-app/src/test/java/com/mycompany/app/KthLargestElementInAnArrayTest.java
@@ -0,0 +1,23 @@
+package com.mycompany.app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jiangmouren on 8/16/17.
+ */
+public class KthLargestElementInAnArrayTest {
+    KthLargestElementInAnArray objectUnderTest = new KthLargestElementInAnArray();
+    int[] array = {3, 2, 1, 5, 6, 4};
+    @Test
+    public void kthLargest() throws Exception {
+        assertEquals(objectUnderTest.kthLargest(2, array), 5);
+    }
+
+    @Test
+    public void kthSmallest() throws Exception {
+        assertEquals(objectUnderTest.kthSmallest(2, array), 2);
+    }
+
+}
